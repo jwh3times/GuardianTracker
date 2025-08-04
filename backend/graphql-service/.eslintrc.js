63e659c85a3ddbd1a2fe1c88773df925db796a05
Jerry Holland <jerryholland00@gmail.com>
2025-08-04 13:09:29 -0400

    Fixes ESLint TypeScript plugin configuration
    Corrects the extends configuration to use the proper plugin syntax for TypeScript ESLint rules, ensuring the linter functions correctly with TypeScript-specific rules and recommendations.

--- backend/graphql-service/.eslintrc.js
+++ backend/graphql-service/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     project: "./tsconfig.json",
   },
   plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "@typescript-eslint/recommended"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   env: {
     node: true,
     es6: true,
