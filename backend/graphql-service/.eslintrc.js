7f039cf8e1d7dcb19333bca413c098c469c300dc
Jerry Holland <jerryholland00@gmail.com>
2025-12-10 00:13:44 -0500

    chore(eslint): set tsconfig root dir for graphql-service

--- backend/graphql-service/.eslintrc.js
+++ backend/graphql-service/.eslintrc.js
@@ -1,9 +1,12 @@
+const path = require("path");
+
 module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: "module",
     project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
   },
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
